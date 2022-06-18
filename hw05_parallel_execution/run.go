61dc0cd5011b714e80731296a9c865f2dbaacbdd
sviatvguss <sviat.v.guss@gmail.com>
2022-06-18 21:53:51 +0600

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,56 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	tasksCh := make(chan Task, n)
+	stopCh := make(chan struct{})
+	doneCh := make(chan struct{})
+	mu := sync.Mutex{}
+	wg := &sync.WaitGroup{}
+
+	go func() {
+		defer close(tasksCh)
+		for _, t := range tasks {
+			tasksCh <- t
+		}
+	}()
+
+	for task := range tasksCh {
+		task := task
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := task()
+			if m > 0 {
+				if err != nil {
+					if m > 1 {
+						mu.Lock()
+						m--
+						mu.Unlock()
+					} else {
+						select {
+						case stopCh <- struct{}{}:
+						default:
+						}
+					}
+				}
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		select {
+		case doneCh <- struct{}{}:
+		default:
+		}
+	}()
+
+	for {
+		select {
+		case <-stopCh:
+			return ErrErrorsLimitExceeded
+		case <-doneCh:
+			return nil
+		}
+	}
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -8,8 +8,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestRun(t *testing.T) {
@@ -66,5 +67,28 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("ignore errors", func(t *testing.T) {
+		tasksCount := 48
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 3
+		maxErrorsCount := -7
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 	})
 }
