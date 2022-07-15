8fd7fceb539c3277e49841166ab1b349451525ad
Sviatoslav Guss <sviat.v.guss@gmail.com>
2022-07-15 20:27:28 +0600

    hw08

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,16 +1,76 @@
 package main
 
-type Environment map[string]EnvValue
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"sync"
+)
 
-// EnvValue helps to distinguish between empty files and files with the first empty line.
-type EnvValue struct {
-	Value      string
-	NeedRemove bool
+type Environment map[string]string
+
+func readLine(file string) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+	line, err := reader.ReadBytes('\n')
+	if err != nil && !errors.Is(err, io.EOF) {
+		return "", err
+	}
+
+	line = bytes.ReplaceAll(line, []byte("\n"), []byte(""))
+	line = bytes.ReplaceAll(line, []byte("\x00"), []byte("\n"))
+
+	return strings.TrimRight(string(line), " \t"), nil
 }
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	var wg sync.WaitGroup
+	var mx sync.Mutex
+
+	env := Environment{}
+
+	fis, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range fis {
+		if !f.Mode().IsRegular() {
+			continue
+		}
+		wg.Add(1)
+		go func(fi os.FileInfo) {
+			var line string
+			var err error
+			defer wg.Done()
+			name := fi.Name()
+			if fi.Size() > 0 {
+				line, err = readLine(path.Join(dir, name))
+				if err != nil {
+					fmt.Printf("can not read data from %v\n", name)
+					return
+				}
+
+			}
+			mx.Lock()
+			env[name] = line
+			mx.Unlock()
+		}(f)
+	}
+	wg.Wait()
+
+	return env, nil
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,54 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zenizh/go-capturer"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("exit codes", func(t *testing.T) {
+		env := Environment{}
+		require.Equal(t, 0, RunCmd([]string{"ls", "testdata"}, env))
+		require.Equal(t, 111, RunCmd([]string{"command-not-exist"}, env))
+	})
+
+	t.Run("apply env vars", func(t *testing.T) {
+		env := Environment{"HELLO": "WORLD"}
+		os.Setenv("PRE", "SOME")
+		output := capturer.CaptureStdout(func() {
+			RunCmd([]string{"env"}, env)
+		})
+		require.Contains(t, output, "PRE=SOME")
+		require.Contains(t, output, "HELLO=WORLD")
+	})
+
+	t.Run("update env vars", func(t *testing.T) {
+		env := Environment{"HELLO": "WORLD"}
+		os.Setenv("HELLO", "NEWWORLD")
+		output := capturer.CaptureStdout(func() {
+			RunCmd([]string{"env"}, env)
+		})
+		require.Contains(t, output, "HELLO=WORLD")
+	})
+
+	t.Run("unset env vars", func(t *testing.T) {
+		env := Environment{"HELLO": ""}
+		os.Setenv("HELLO", "VAL")
+		output := capturer.CaptureStdout(func() {
+			RunCmd([]string{"env"}, env)
+		})
+		require.NotContains(t, output, "HELLO")
+	})
+
+	t.Run("unset env var, skip setting value, if =", func(t *testing.T) {
+		env := Environment{"HELLO": "SOME=VAL"}
+		os.Setenv("HELLO", "VAL")
+		output := capturer.CaptureStdout(func() {
+			RunCmd([]string{"env"}, env)
+		})
+		require.NotContains(t, output, "HELLO")
+	})
 }
